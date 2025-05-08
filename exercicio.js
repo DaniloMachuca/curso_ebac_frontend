5f65389b33b7bcc933d57fccedad601ee7a9c57d
Danilo Machuca de Souza <danilo.machuca111@gmail.com>
2025-05-08 13:30:33 -0300

    exercicio feito

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function dizOi(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicar(2, 3));
+console.log(dizOi("seuNome"));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b: number) {
+  return a * b;
+}
+
+function dizOi(nome: string) {
+  return `Olá ${nome}`;
+}
+
+console.log(multiplicar(2, 3));
+console.log(dizOi("seuNome"));
